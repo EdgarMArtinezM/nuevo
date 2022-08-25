bcbe23a4d5d041fb5ab2beb624736d319f6ebea3
EdgarMArtinezM <edgar060498@gmail.com>
2022-08-25 18:35:23 -0500

    Hola

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ejemplo';
+  numero = 0;
+  constructor(){
+    setInterval(() => {
+      this.numero++
+    }, 100);
+  }
 }
